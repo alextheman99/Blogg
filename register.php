9ffbf798a30f61824557032c57199aeecc781ed2
Alexander Björk Wahlberg <alexander.bjorkwahl@learnet.se>
2018-06-02 01:41:14 +0200

    Rensat

--- register.php
+++ register.php
@@ -3,65 +3,59 @@
    and sends account confirmation email message
  */
 
-// Set session variables to be used on profile.php page
+/* Set session variables to be used on profile.php page */
 $_SESSION['email'] = $_POST['email'];
 $_SESSION['first_name'] = $_POST['firstname'];
 $_SESSION['last_name'] = $_POST['lastname'];
 
-// Escape all $_POST variables to protect against SQL injections
+/* Escape all $_POST variables to protect against SQL injections */
 $first_name = $mysqli->escape_string($_POST['firstname']);
 $last_name = $mysqli->escape_string($_POST['lastname']);
 $email = $mysqli->escape_string($_POST['email']);
 $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
 
-// Check if user with that email already exists
+/* Check if user with that email already exists */
 $result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
 
-// We know user email exists if the rows returned are more than 0
+/* We know user email exists if the rows returned are more than 0 */
 if ( $result->num_rows > 0 ) {
-
     $_SESSION['message'] = 'User with this email already exists!';
     header("location: error.php");
-
-}
-else { // Email doesn't already exist in a database, proceed...
-
-    // active is 0 by DEFAULT (no need to include it here)
+} else {
     $sql = "INSERT INTO users (first_name, last_name, email, password, hash) "
             . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
 
-    // Add user to the database
-    if ( $mysqli->query($sql) ){
-
-        $_SESSION['admin'] = 0;
-        $_SESSION['active'] = 0; //0 until user activates their account with verify.php
-        $_SESSION['logged_in'] = true; // So we know the user has logged in
-        $_SESSION['message'] =
+    /* Add user to the database */
+if ( $mysqli->query($sql) ){
+    $_SESSION['admin'] = 0; //0 so that users don't get admin priviliges
+    $_SESSION['active'] = 0; //0 until user activates their account with verify.php
+    $_SESSION['logged_in'] = true; // So we know the user has logged in
+    $_SESSION['message'] =
 
                  "Confirmation link has been sent to $email, please verify
                  your account by clicking on the link in the message!";
 
-        // Send registration confirmation link (verify.php)
-        $to      = $email;
-        $subject = 'Account Verification ( clevertechie.com )';
-        $message_body = '
-        Hello '.$first_name.',
+        /* Send registration confirmation link. */
+    $to      = $email;
+    $subject = 'Account Verification ( clevertechie.)';
+    $message_body = '
+    Hej '.$first_name.',
 
-        Thank you for signing up!
+    Tack för att du har registrerat dig!
 
-        Please click this link to activate your account:
+    Var vänlig att klicka på den här länken för att aktivera kontot:
 
-        http://localhost/login-system/verify.php?email='.$email.'&hash='.$hash;
+    http://localhost/login-system/verify.email='.$email.'&hash='.$hash;
 
-        mail( $to, $subject, $message_body );
+    mail( $to, $subject, $message_body );
 
-        header("location: profile.php");
+    header("location: profile.php");
 
     }
 
     else {
-        $_SESSION['message'] = 'Registration failed!';
+        $_SESSION['message'] = 'Registreringen misslyckades!';
         header("location: error.php");
     }
 
